189558bca65f1dfe70bc08ef4c68fb9c4bc6c0f5
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-10-20 11:12:34 -0500

    agregar pagina not found, componente boton y finalizar proyecto

---
+++ src/components/ButtonBackToHome.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const ButtonBackToHome=()=>(
+    <Link to='/' className="button is-primary is-outlined" >Back To Home</Link>
+)
---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React  from 'react'
+import { ButtonBackToHome } from '../components/ButtonBackToHome'
+
+export const NotFound = () => (
+
+    <div>
+        <h1 style={{color:'#fff', fontSize:'50px'}} className="title">404!</h1>
+        <h2 style={{color:'#fff'}} className="subtitle">PAGE NOT FOUND</h2>
+        <ButtonBackToHome/>
+    </div>
+
+
+)
+    
